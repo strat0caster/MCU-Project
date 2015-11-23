7221e46632a9ba5334c66499373511a0bffd7daf
strat0caster <IsaacBing@gmail.com>
2015-11-23 21:49:26 +0800

    Added two headers

---
+++ lights.h
@@ -0,0 +1,31 @@
+#ifndef _LIGHTS_H_
+#define _LIGHTS_H_
+
+//mainly just adding all sorts of header files in
+
+#include "reg51.h"
+#include <MPU6050.h>
+// downloaded already from github;
+
+#include <speed.h>
+// speed obtaining module
+// contains interrupt functions used or renewing the angular speed of the wheel
+
+#include <led.h>
+// led pattern module
+// contains functions for displaying different patterns.
+
+#include <miles.h>
+// mileage module
+// contains functions for integrating speed to get the mileage/distance.
+
+#include <turning.h>
+// turning status module
+// contains turning status data obtaining and corresponding interrupt function
+
+
+//#include <supersonic.h>
+// reserved for the supersonic distance measuring and alarming module;
+// only de-comment if progress allows 
+
+#endif
