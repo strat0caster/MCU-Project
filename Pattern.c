8b89d6699c14eb4d1430905cb4219299f32e4d50
SamSongDi <3130102292@zju.edu.cn>
2015-12-30 15:34:27 +0800

    图案设计 pattern.c
    Pattern_Cell中角度规范到0到360

--- Pattern.c
+++ Pattern.c
@@ -6,3 +6,22 @@
 float wx,wy,wz,ax,ay,az;//raw data from MPU6050;quite likely that we won't use wy&ay though
 float miles=0;
 float delta_miles;
+
+void Pattern_1(int start){
+	Pattern_Cell(start,start+180,0xFF,0);
+}
+
+void Pattern_11(){
+	for(i=0;i<36;i++){
+		Pattern_Cell(i*10,4+i*10,0,1);
+		Pattern_Cell(i*10,4+i*10,1,1);
+		Pattern_Cell(i*10,4+i*10,2,1);
+		Pattern_Cell(i*10,4+i*10,3,1);
+	}
+	for(i=0;i<72;i++){
+		Pattern_Cell(i*5,4+i*5,4,1);
+		Pattern_Cell(i*5,4+i*5,5,1);
+		Pattern_Cell(i*5,4+i*5,6,1);
+		Pattern_Cell(i*5,4+i*5,7,1);
+	}
+}
--- Pattern_Cell_Test/Pattern_Cell_Test.c
+++ Pattern_Cell_Test/Pattern_Cell_Test.c
@@ -3,6 +3,7 @@
 
 int LED[3]={0,120,240}	  ;//表示LED的位置
 float Yaw;
+int C1[8]={0xFE,0xFD,0xFB,0xF7,0xEF,0xDF,0xBF,0x7F};
 
 unsigned char C,D;
 
@@ -16,8 +17,8 @@ void Pattern_Cell(int A,int B,unsigned char C,unsigned char D)
 {
 	int i;
 	for(i=0;i<NUM;i++){
-		LED[i]=Yaw+i*360/NUM;
-		for(;LED[i]<-180||LED[i]>=180;)
+		LED[i]=Yaw+i*360/NUM+180;
+		for(;LED[i]<0||LED[i]>=360;)
 			LED[i]-=360;		//控制所有坐标在-180到180之间
 	}
 
@@ -26,38 +27,38 @@ void Pattern_Cell(int A,int B,unsigned char C,unsigned char D)
 		if(LED[0]>=A&&LED[0]<B)
 			P0=C;
 		else
-			P0=0x00;
+			P0=0xFF;
 
 		if(LED[1]>=A&&LED[1]<B)
 			P1=C;
 		else
-			P1=0x00;
+			P1=0xFF;
 
 		if(LED[2]>=A&&LED[2]<B)
 			P2=C;
 		else
-			P2=0x00;
+			P2=0xFF;
 	}
 
 	//D=0时分个单独调整
-	else{
-		int C1[8]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+	else if(D==0){
 		
 		if(LED[0]>=A&&LED[0]<B)
 			P0=C1[C];
 		else
-			P0=0x00;
+			P0=0xFF;
 
 		if(LED[1]>=A&&LED[1]<B)
 			P1=C1[C];
 		else
-			P1=0x00;
+			P1=0xFF;
 
 		if(LED[2]>=A&&LED[2]<B)
 			P2=C1[C];
 		else
-			P2=0x00;
+			P2=0xFF;
 	}
+
 }
 
 int main()
