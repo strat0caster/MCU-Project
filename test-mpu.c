5bd6e8bf6c1baa7f14a7787adec5b20583a45839
strat0caster <IsaacBing@gmail.com>
2016-01-05 12:39:34 +0800

    115200baud
    115200baud sucess by T2

--- test-mpu.c
+++ test-mpu.c
@@ -4,9 +4,8 @@ sfr AUXR=0x8E;
 sfr T2CON=0xC8;
 sfr RCAP2L=0xCA;
 sfr RCAP2H=0xCB;
-sfr T2L=0xCC;
-sfr T2H=0xCD;
-#define BAUD 115200
+sfr TL2=0xCC;
+sfr TH2=0xCD;
 
 int a[3],w[3],Angle[3]; 
 //Angle[0]=Angle_X, Angle[1]=Angle_Y, Angle[2]=Angle_Z;
@@ -21,18 +20,16 @@ void main(void) {
 	void UartInit();
 	UartInit();
 	while(1){
-/*			if( Angle[2] >21800){
-				LED=0x55;
+			if( Angle[2] >21800){
+				LED=0x00;
 			}
 			else{
-				LED=0x33;
+				LED=0xFF;
 			}
 			low_8=Angle[2] & 0xFF;
 			high_8=(Angle[2]>>8)&0xFF;
 			P0=LED;
-			P1=low_8;
-			P2=high_8;
-*/
+
 	}
 
 }
@@ -57,8 +54,10 @@ void UartInit(void)		//9600bps@11.0592MHz
 //以上为UART方式1，9600波特率；已经测试成功，可以收到并中断;TL1!!!!!
 */
 	SCON = 0x50;		//8???,?????
-	T2L = 0xFE;		//??????
-	T2H = 0xFF;		//??????
+	RCAP2L = 0xFD;
+	RCAP2H = 0xFF;
+	TL2 = 0xFD;		//??????
+	TH2 = 0xFF;		//??????
 	T2CON=0x34;
 
 }
@@ -67,7 +66,7 @@ void ser() interrupt 4
 {
 	if (RI)
 	{
-/*		RI=0;	
+		RI=0;	
 		Receive_Buff[counter]=SBUF;	
 		
 	  if(counter==0&&Receive_Buff[0]!=0x55) return;      //第0号数据不是帧头
@@ -79,29 +78,24 @@ void ser() interrupt 4
 		
 			switch(Receive_Buff [1])
 			{
-			case 0x51:
-			a[0]=(Receive_Buff[3]<<8|Receive_Buff[2])/32768.0*16;
-			a[1]=(Receive_Buff[5]<<8|Receive_Buff[4])/32768.0*16;
-			a[2]=(Receive_Buff[7]<<8|Receive_Buff[6])/32768.0*16;
-			break;
-			case 0x52:		
-			w[0]=(Receive_Buff[3]<<8|Receive_Buff[2])/32768.0*2000;
-			w[1]=(Receive_Buff[5]<<8|Receive_Buff[4])/32768.0*2000;
-			w[2]=(Receive_Buff[7]<<8|Receive_Buff[6])/32768.0*2000;
-			break;
+//			case 0x51:
+//			a[0]=(Receive_Buff[3]<<8|Receive_Buff[2])/32768.0*16;
+//			a[1]=(Receive_Buff[5]<<8|Receive_Buff[4])/32768.0*16;
+//			a[2]=(Receive_Buff[7]<<8|Receive_Buff[6])/32768.0*16;
+//			break;
+//			case 0x52:		
+//			w[0]=(Receive_Buff[3]<<8|Receive_Buff[2])/32768.0*2000;
+//			w[1]=(Receive_Buff[5]<<8|Receive_Buff[4])/32768.0*2000;
+//			w[2]=(Receive_Buff[7]<<8|Receive_Buff[6])/32768.0*2000;
+//			break;
 			case 0x53: 
 //			Angle[0]=(Receive_Buff[3]<<8|Receive_Buff[2])/32768.0*180;
-//			Angle[1]=(Receive_Buff[5]<<8|Receive_Buff[4])/32768.0*180;
-			Angle[2]=(Receive_Buff[7]<<8|Receive_Buff[6]);
+			Angle[1]=(Receive_Buff[5]<<8|Receive_Buff[4])/32768.0*180;
+//			Angle[2]=(Receive_Buff[7]<<8|Receive_Buff[6]);
 			break;
 			default: break;
 			} 
 	      }
-				*/
-				
-		RI=0;
-		ACC=SBUF;
-		SBUF=ACC;
 
 	  }
 	
